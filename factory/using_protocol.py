e9321d649327fe53dfade3cb0475d94609ff2edc
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-10-25 00:07:19 +0300

    Update using_protocol.py

--- factory/using_protocol.py
+++ factory/using_protocol.py
@@ -1,96 +1,102 @@
 """
 Basic video exporting example
 """
-import pathlib
-from typing import Protocol
+from pathlib import Path
+from abc import ABC, abstractmethod
 
 
-class VideoExporter(Protocol):
+class VideoExporter(ABC):
   """Basic representation of video exporting codec."""
 
+  @abstractmethod
   def prepare_export(self, video_data):
     """Prepares video data for exporting."""
 
-  def do_export(self, folder: pathlib.Path):
+  @abstractmethod
+  def do_export(self, folder: Path):
     """Exports the video data to a folder."""
 
 
-class LosslessVideoExporter:
+class LosslessVideoExporter(VideoExporter):
   """Lossless video exporting codec."""
 
   def prepare_export(self, video_data):
     print("Preparing video data for lossless export.")
 
-  def do_export(self, folder: pathlib.Path):
+  def do_export(self, folder: Path):
     print(f"Exporting video data in lossless format to {folder}.")
 
 
-class H264BPVideoExporter:
+class H264BPVideoExporter(VideoExporter):
   """H.264 video exporting codec with Baseline profile."""
 
   def prepare_export(self, video_data):
     print("Preparing video data for H.264 (Baseline) export.")
 
-  def do_export(self, folder: pathlib.Path):
+  def do_export(self, folder: Path):
     print(f"Exporting video data in H.264 (Baseline) format to {folder}.")
 
 
-class H264Hi422PVideoExporter:
+class H264Hi422PVideoExporter(VideoExporter):
   """H.264 video exporting codec with Hi422P profile (10-bit, 4:2:2 chroma sampling)."""
 
   def prepare_export(self, video_data):
     print("Preparing video data for H.264 (Hi422P) export.")
 
-  def do_export(self, folder: pathlib.Path):
+  def do_export(self, folder: Path):
     print(f"Exporting video data in H.264 (Hi422P) format to {folder}.")
 
 
-class AudioExporter(Protocol):
+class AudioExporter(ABC):
   """Basic representation of audio exporting codec."""
 
+  @abstractmethod
   def prepare_export(self, audio_data):
     """Prepares audio data for exporting."""
 
-  def do_export(self, folder: pathlib.Path):
+  @abstractmethod
+  def do_export(self, folder: Path):
     """Exports the audio data to a folder."""
 
 
-class AACAudioExporter:
+class AACAudioExporter(AudioExporter):
   """AAC audio exporting codec."""
 
   def prepare_export(self, audio_data):
     print("Preparing audio data for AAC export.")
 
-  def do_export(self, folder: pathlib.Path):
+  def do_export(self, folder: Path):
     print(f"Exporting audio data in AAC format to {folder}.")
 
 
-class WAVAudioExporter:
+class WAVAudioExporter(AudioExporter):
   """WAV (lossless) audio exporting codec."""
 
   def prepare_export(self, audio_data):
     print("Preparing audio data for WAV export.")
 
-  def do_export(self, folder: pathlib.Path):
+  def do_export(self, folder: Path):
     print(f"Exporting audio data in WAV format to {folder}.")
 
 
-class ExporterFactory(Protocol):
+class ExporterFactory(ABC):
   """
   Factory that represents a combination of video and audio codecs.
   The factory doesn't maintain any of the instances it creates.
   """
 
+  @abstractmethod
   def get_video_exporter(self) -> VideoExporter:
     """Returns a new video exporter instance."""
     pass
 
+  @abstractmethod
   def get_audio_exporter(self) -> AudioExporter:
     """Returns a new audio exporter instance."""
     pass
 
 
-class FastExporter:
+class FastExporter(ExporterFactory):
   """Factory aimed at providing a high speed, lower quality export."""
 
   def get_video_exporter(self) -> VideoExporter:
@@ -100,7 +106,7 @@ class FastExporter:
     return AACAudioExporter()
 
 
-class HighQualityExporter:
+class HighQualityExporter(ExporterFactory):
   """Factory aimed at providing a slower speed, high quality export."""
 
   def get_video_exporter(self) -> VideoExporter:
@@ -120,33 +126,31 @@ class MasterQualityExporter(ExporterFactory):
     return WAVAudioExporter()
 
 
+FACTORIES = {
+  "low": FastExporter(),
+  "high": HighQualityExporter(),
+  "master": MasterQualityExporter(),
+}
+
 # Helper function
-def read_exporter() -> ExporterFactory:
+def read_factory() -> ExporterFactory:
   """
   Constructs an exporter factory based on the user's preference.
   Returns: An exporter factory.
   """
-  factories = {
-      "low": FastExporter(),
-      "high": HighQualityExporter(),
-      "master": MasterQualityExporter()
-  }
   # read the desired export quality
   while True:
     export_quality = input(
-        "Enter desired output quality (low, high, master): ")
-    if export_quality in factories:
-      return factories[export_quality]
-    print(f"Unknown output quality option: {export_quality}!")
+        f"Enter desired output quality ({', '.join(FACTORIES)}): ")
+    try:
+        return FACTORIES[export_quality]
+    except KeyError:
+        print(f"Unknown output quality option: {export_quality}!")
 
+def do_export(fac: ExporterFactory) -> None:
+  """Do a test export using a video and audio exporters."""
 
-# providing the factory as an argument to the main function,
-# adds more flexability than invoking the factory directly in the main.
-# This best practice is called (Dependency Injection).
-def main(fac: ExporterFactory) -> None:
-  """Main function."""
-
-  # retrieve the video and audio exporters
+  # retrieve the exporters
   video_exporter = fac.get_video_exporter()
   audio_exporter = fac.get_audio_exporter()
 
@@ -155,12 +159,18 @@ def main(fac: ExporterFactory) -> None:
   audio_exporter.prepare_export("placeholder_for_audio_data")
 
   # do the export
-  folder = pathlib.Path("/usr/tmp/video")
+  folder = Path("/usr/tmp/video")
   video_exporter.do_export(folder)
   audio_exporter.do_export(folder)
 
-
+def main() -> None:
+  """Main function."""
+  
+  # create the factory
+  factory = read_factory()
+  
+  # perform the exporting job
+  do_export(factory)
+  
 if __name__ == "__main__":
-  fac = read_exporter()
-  main(fac)
-
+  main()
