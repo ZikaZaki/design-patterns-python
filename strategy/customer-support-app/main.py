78504ed7f3cf111bb03ff2d0a5b3f600a372ba6a
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-10-27 22:09:35 +0300

    Update main.py

--- strategy/customer-support-app/main.py
+++ strategy/customer-support-app/main.py
@@ -1,1 +1,20 @@
+from support.app import CustomerSupport
+from support.ticket import SupportTicket
 
+
+
+def main():
+  # create the application
+  app = CustomerSupport()
+
+  # register a few tickets
+  app.add_ticket(SupportTicket("Zack Ali", "My computer makes strang sounds!"))
+  app.add_ticket(SupportTicket("Linus Sebastian", "I can't upload any videos, please help."))
+  app.add_ticket(SupportTicket("John Smith", "VSCode doesn't automatically solve my bugs."))
+
+  # process the tickets
+  app.process_tickets("fifo")
+
+
+if __name__ == "__main__":
+  main()
