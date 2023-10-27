223349363f4d6f59bc8073e77c88bec564d87465
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-10-28 01:26:49 +0300

    Update main.py

--- strategy/customer-support-app/main.py
+++ strategy/customer-support-app/main.py
@@ -1,6 +1,11 @@
-from support.app import CustomerSupport
+from support.app import CustomerSupport, FILOOrderingStrategy, RadnomOrderingStrategy, random_strategy_creator
 from support.ticket import SupportTicket
 
+
+class BlackHoleStrategy:
+
+  def __call__(self, tickets: list[SupportTicket]) -> list[SupportTicket]:
+    return []
 
 
 def main():
@@ -9,11 +14,15 @@ def main():
 
   # register a few tickets
   app.add_ticket(SupportTicket("Zack Ali", "My computer makes strang sounds!"))
-  app.add_ticket(SupportTicket("Linus Sebastian", "I can't upload any videos, please help."))
-  app.add_ticket(SupportTicket("John Smith", "VSCode doesn't automatically solve my bugs."))
+  app.add_ticket(
+      SupportTicket("Linus Sebastian",
+                    "I can't upload any videos, please help."))
+  app.add_ticket(
+      SupportTicket("John Smith",
+                    "VSCode doesn't automatically solve my bugs."))
 
   # process the tickets
-  app.process_tickets("fifo")
+  app.process_tickets(random_strategy_creator(seed=1))
 
 
 if __name__ == "__main__":
