af43a6ddf93b6be3c7804b38ee57270c02033737
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-10-28 01:26:30 +0300

    Update app.py

--- strategy/customer-support-app/support/app.py
+++ strategy/customer-support-app/support/app.py
@@ -1,29 +1,74 @@
+from collections.abc import Callable
+from dataclasses import dataclass
 import random
+# from abc import ABC, abstractmethod
+from typing import Optional, Protocol
 
 from support.ticket import SupportTicket
 
+# we can also use the Callable to define the type of TicketOrderingStrategy
+TicketOrderingStrategy = Callable[[list[SupportTicket]], list[SupportTicket]]
+
+# class TicketOrderingStrategy(Protocol):
+
+#   # def create_ordering(self, tickets: list[SupportTicket]) -> list[SupportTicket]:
+#   def __call__(self, tickets: list[SupportTicket]) -> list[SupportTicket]:
+#     """Returns an ordered list of tickets."""
+#     ...
+
+
+class FIFOOrderingStrategy:
+
+  def __call__(self, tickets: list[SupportTicket]) -> list[SupportTicket]:
+    return tickets.copy()
+
+
+class FILOOrderingStrategy:
+
+  def __call__(self, tickets: list[SupportTicket]) -> list[SupportTicket]:
+    return list(reversed(tickets))
+
+
+# a way to add extra parameters with functions is using closures
+def random_strategy_creator(seed: Optional[int] = None) -> TicketOrderingStrategy:
+  def random_strategy(tickets: list[SupportTicket]) -> list[SupportTicket]:
+    random.seed(seed)
+    return random.sample(tickets, len(tickets))
+
+  return random_strategy
+
+
+# to add extra parameters like settings/configs
+@dataclass
+class RadnomOrderingStrategy:
+  seed: Optional[int] = None
+
+  def __call__(self, tickets: list[SupportTicket]) -> list[SupportTicket]:
+    random.seed(self.seed)
+    return random.sample(tickets, len(tickets))
+
 
 class CustomerSupport:
+
   def __init__(self):
     self.tickets: list[SupportTicket] = []
 
   def add_ticket(self, ticket: SupportTicket):
     self.tickets.append(ticket)
 
-  def process_tickets(self, processing_strategy: str = "fifo"):
+  def process_tickets(self, processing_strategy: TicketOrderingStrategy):
+    # create the ordered list of tickets
+    ticket_list = processing_strategy(self.tickets)
+
     # if it's empty, don't do anything
-    if len(self.tickets) == 0:
+    if len(ticket_list) == 0:
       print("There are no tickets to process. Well done!")
       return
 
-    if processing_strategy == "fifo":
-      for ticket in self.tickets:
-        ticket.process()
-    elif processing_strategy == "filo":
-      for ticket in reversed(self.tickets):
-        ticket.process()
-    elif processing_strategy == "random":
-      random_list = random.sample(self.tickets, len(self.tickets))
-      for ticket in random_list:
-        ticket.process()
-        
+    # go through the tickets list and process each ticket
+    for ticket in ticket_list:
+      print(f"Processing ticket {ticket.id}...")
+      ticket.process()
+
+    # clear the tickets list
+    self.tickets = []
